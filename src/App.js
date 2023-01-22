6246a03aff53981c8f208e409d8b2153cb5c73a5
Siful Bappy <bappy.ac.bd@gmail.com>
2023-01-23 00:05:04 +0900

    almost complete the bottom menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import MenuContainer from './components/ManuContainer';
+import { AccountBalanceWalletRounded, Chat, Favorite, HomeRounded, Settings, SummarizeRounded } from '@mui/icons-material';
 
 function App() {
   return (
     <div className="App">
       {/* Header Section  */}
       <Header></Header>
+
       {/* Main Body  */}
+      <main>
+        <div className="mainContainer"></div>
+        <div className="rightMenu"></div>
+      </main>
 
       {/* Bottom menu  */}
+      <div className="bottomMenu">
+        <ul id="menu">
+          <MenuContainer link={"#"} icon={<HomeRounded></HomeRounded>}></MenuContainer>
+          <MenuContainer link={"#"} icon={<Chat></Chat>}></MenuContainer>
+          <MenuContainer link={"#"} icon={<AccountBalanceWalletRounded></AccountBalanceWalletRounded>}></MenuContainer>
+          <MenuContainer link={"#"} icon={<Favorite></Favorite>}></MenuContainer>
+          <MenuContainer link={"#"} icon={<SummarizeRounded></SummarizeRounded>}></MenuContainer>
+          <MenuContainer link={"#"} icon={<Settings></Settings>}></MenuContainer>
+          <div className="indicator"></div>
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/components/ManuContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MenuContainer = ({link, icon}) => {
+    return (
+        <li>
+            <a href={link}>
+                <span className='icon'>{icon}</span>
+            </a>
+        </li>
+    );
+};
+
+export default MenuContainer;
